35b2915d53a7bc537e081778ba1d75d57172a1a6
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2020-02-21 01:11:13 +0800

    JavaDoc

--- src/jsMain/kotlin/ru/github/onotoliy/geojson/utils.kt
+++ src/jsMain/kotlin/ru/github/onotoliy/geojson/utils.kt
@@ -2,4 +2,10 @@ package ru.github.onotoliy.geojson
 
 import kotlin.reflect.KClass
 
+/**
+ * Получение названия класса.
+ *
+ * @param clazz Класс.
+ * @return Названия класса.
+ */
 actual fun <T : Any> getSimpleClassName(clazz: KClass<T>): String = clazz.js.name
